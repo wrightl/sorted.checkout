fa160ef421d3d570e5aad1a2ca40009b483ae560
Lee Wright <lee.wright@servelec.co.uk>
2021-03-21 17:25:30 +0000

    disabled https redirect to make testing easier

--- Checkout/Startup.cs
+++ Checkout/Startup.cs
@@ -66,7 +66,7 @@ namespace Checkout
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Checkout v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
